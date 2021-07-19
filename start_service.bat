79cb5ac14742b246e963dd8079fc8577fd9ce5b3
TeamEmilia <87614680+TeamEmilia@users.noreply.github.com>
2021-07-19 09:31:53 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop EmiliaRobot
-net start EmiliaRobot
+net stop TheEmiliaBot
+net start TheEmiliaBot
